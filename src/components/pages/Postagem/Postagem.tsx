83d0e66dd3948b7691c2df84a4369befd818ac46
joashneves <joashneves@gmail.com>
2024-07-04 13:13:48 -0300

    sera que agora vai?

--- src/components/pages/Postagem/Postagem.tsx
+++ src/components/pages/Postagem/Postagem.tsx
@@ -1,33 +1,40 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { useParams } from "react-router-dom";
 import Hud from "../../molecules/Hud/Hud";
 import Cabecalho from "../../templates/cabecalho/Cabecalho";
 import styles from "./Postagem.module.css";
 
+// Lista de arquivos disponíveis (substitua isso pela lógica de buscar do servidor se necessário)
+const arquivosDisponiveis = [
+  'post0.md',
+  'post1.md',
 
-
-
+  // Adicione outros arquivos conforme necessário
+];
 
 const Postagem = () => {
   const { id } = useParams();
+  const [mdxContent, setMdxContent] = useState<string | null>(null);
 
-  // Importa dinamicamente o conteúdo do arquivo MDX como texto
-  const [mdxContent, setMdxContent] = React.useState<string | null>(null);
-
-  React.useEffect(() => {
+  useEffect(() => {
     // Função para importar o conteúdo do arquivo MDX como texto
     const fetchMDXContent = async () => {
       try {
-        const response = await fetch(`https://blog-eosin-pi-22.vercel.app/public/publicacao/post${id}.md`);
+        const arquivoEncontrado = arquivosDisponiveis.find(arquivo => arquivo === `post${id}.md`);
+        if (!arquivoEncontrado) {
+          throw new Error(`Arquivo post${id}.md não encontrado`);
+        }
+
+        const response = await fetch(`/public/publicacao/${arquivoEncontrado}`);
         if (!response.ok) {
-          throw new Error(`Failed to fetch MDX content for post ${id}`);
+          throw new Error(`Falha ao buscar conteúdo MDX para o post ${id}`);
         }
         const mdxText = await response.text();
         setMdxContent(mdxText);
       } catch (error) {
-        console.error("Error fetching MDX content:", error);
-        // Trate o erro como achar adequado
+        console.error("Erro ao buscar conteúdo MDX:", error);
+        // Trate o erro conforme necessário
       }
     };
 
@@ -39,13 +46,12 @@ const Postagem = () => {
       <Hud />
       <article className={styles.postagem}>
         <div className={styles.content}>
-          {mdxContent && <ReactMarkdown children={mdxContent} />}
+          {mdxContent ? <ReactMarkdown>{mdxContent}</ReactMarkdown> : <div>Carregando...</div>}
         </div>
       </article>
       <Cabecalho />
     </>
   );
 };
-
 
 export default Postagem;
