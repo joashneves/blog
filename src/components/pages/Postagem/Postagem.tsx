798d480f6ec8e71a469428affe778cc31da09768
joashneves <joashneves@gmail.com>
2024-07-04 13:45:28 -0300

    Eu vou me matar 00:00

--- src/components/pages/Postagem/Postagem.tsx
+++ src/components/pages/Postagem/Postagem.tsx
@@ -1,44 +1,29 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { useParams } from "react-router-dom";
 import Hud from "../../molecules/Hud/Hud";
 import Cabecalho from "../../templates/cabecalho/Cabecalho";
 import styles from "./Postagem.module.css";
 
-// Lista de arquivos disponíveis (substitua isso pela lógica de buscar do servidor se necessário)
-const arquivosDisponiveis = [
-  'post0.md',
-  'post1.md',
-
-  // Adicione outros arquivos conforme necessário
-];
-
-const Postagem = () => {
-  const { id } = useParams();
-  const [mdxContent, setMdxContent] = useState<string | null>(null);
+const Postagem: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [markdownContent, setMarkdownContent] = useState<string>("");
 
   useEffect(() => {
-    // Função para importar o conteúdo do arquivo MDX como texto
-    const fetchMDXContent = async () => {
+    const fetchMarkdown = async () => {
       try {
-        const arquivoEncontrado = arquivosDisponiveis.find(arquivo => arquivo === `post${id}.md`);
-        if (!arquivoEncontrado) {
-          throw new Error(`Arquivo post${id}.md não encontrado`);
-        }
-
-        const response = await fetch(`/public/publicacao/${arquivoEncontrado}`);
+        const response = await fetch(`/public/publicacao/post${id}.md`);
         if (!response.ok) {
-          throw new Error(`Falha ao buscar conteúdo MDX para o post ${id}`);
+          throw new Error(`Failed to fetch markdown content for post ${id}`);
         }
-        const mdxText = await response.text();
-        setMdxContent(mdxText);
+        const text = await response.text();
+        setMarkdownContent(text);
       } catch (error) {
-        console.error("Erro ao buscar conteúdo MDX:", error);
-        // Trate o erro conforme necessário
+        console.error("Error fetching markdown content:", error);
       }
     };
 
-    fetchMDXContent();
+    fetchMarkdown();
   }, [id]);
 
   return (
@@ -46,7 +31,7 @@ const Postagem = () => {
       <Hud />
       <article className={styles.postagem}>
         <div className={styles.content}>
-          {mdxContent ? <ReactMarkdown>{mdxContent}</ReactMarkdown> : <div>Carregando...</div>}
+          <ReactMarkdown>{markdownContent}</ReactMarkdown>
         </div>
       </article>
       <Cabecalho />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mdx from 'vite-plugin-mdx';
+
 
 export default defineConfig({
   plugins: [
-    react(),
+    react()
   ],
+  assetsInclude: ['**/*.md']
 });
-
