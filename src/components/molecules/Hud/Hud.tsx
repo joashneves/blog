bc293139ad256634d9d9ab6a03752cc3848a4292
Joashneves@gmail.com <joashneves@gmail.com>
2024-09-15 17:33:19 -0300

    aff cansei, tentando arruma celular

--- src/components/molecules/Hud/Hud.tsx
+++ src/components/molecules/Hud/Hud.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import styles from './Hud.module.css';
 
 interface HudProps {
@@ -10,19 +11,39 @@ interface HudProps {
 }
 
 const Hud: React.FC<HudProps> = ({
-      inicio = 'Inicio',
-      posts = 'Posts',
-      project = 'Portifolio',
-      contato = 'Contato'
-    }) => {
+  inicio = 'Inicio',
+  posts = 'Posts',
+  project = 'Portifolio',
+  contato = 'Contato'
+}) => {
+  // Definir o breakpoint para celulares com largura máxima de 412px
+  const isMobile = useMediaQuery({ query: '(max-width: 412px)' });
+
+  // Estado para controlar a visibilidade do menu em dispositivos móveis
+  const [isMenuVisible, setIsMenuVisible] = useState(!isMobile);
+
+  // Função para alternar a visibilidade do menu
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
   return (
     <header className={styles.cabecario}>
-      <div>
-        <Link className={styles.link} to='/'>{inicio}</Link>
-        <Link className={styles.link} to='/Post'>{posts}</Link>
-        <Link className={styles.link} to='/Project'>{project}</Link>
-        <Link className={styles.link} to='/Contato'>{contato}</Link>
-      </div>
+      {/* Mostrar o botão "hamburger" apenas em dispositivos móveis */}
+      {isMobile && (
+        <button className={styles.hamburger} onClick={toggleMenu}>
+          ☰ {/* Ícone do menu hamburger */}
+        </button>
+      )}
+
+      {isMenuVisible ? (
+      <div className={isMobile ? styles.linkLateral  : ''}>
+        <Link className={styles.link} to='/' onClick={toggleMenu}>{inicio}</Link>
+        <Link className={styles.link} to='/Post' onClick={toggleMenu}>{posts}</Link>
+        <Link className={styles.link} to='/Project' onClick={toggleMenu}>{project}</Link>
+        <Link className={styles.link} to='/Contato' onClick={toggleMenu}>{contato}</Link>
+      </div>) : (<></>)
+      }
     </header>
   );
 }
